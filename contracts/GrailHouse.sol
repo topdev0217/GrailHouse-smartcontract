a871af57c0c0e9fc4f433c9be50b9bbcf7dd01b2
topdev0217 <77609369+topdev0217@users.noreply.github.com>
2022-08-03 10:10:56 -0400

    complete contract(erc-721)

--- contracts/GrailHouse.sol
+++ contracts/GrailHouse.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: MIT
 
-
 pragma solidity ^0.8.4;
 
 import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-pragma solidity ^0.8.0;
-
-contract GrailHouse is ERC721AUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {  
+contract GrailHouse is ERC721AUpgradeable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    uint256 public constant MAX_ELEMENTS = 15;
     bool public MINTING_PAUSED;
+    string public baseTokenURI;
+    string public _contractURI;
+
 
     function initialize() initializerERC721A initializer public {
         __ERC721A_init("GrailHouse", "GRAILHOUSE");
         __Ownable_init();
 
         MINTING_PAUSED = true;
+        _contractURI = "";
     }
 
     function setPauseMinting(bool _pause) public onlyOwner { 
@@ -24,8 +26,25 @@ contract GrailHouse is ERC721AUpgradeable, OwnableUpgradeable, ReentrancyGuardUp
     }
 
     function publicMint() external nonReentrant {
-        require(!MINTING_PAUSED, "Minting is not active");       
+        require(!MINTING_PAUSED, "Minting is not active");
+        require(totalSupply() < MAX_ELEMENTS, 'All tokens have been minted');
 
         _mint(msg.sender, 1);
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    function setBaseURI(string calldata baseURI) public onlyOwner {
+        baseTokenURI = baseURI;
+    }
+
+    function setContractURI(string calldata URI) external onlyOwner {
+        _contractURI = URI;
+    }
+
+    function contractURI() public view returns (string memory) {
+        return _contractURI;
     }
 }
